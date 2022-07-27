f4169bcf88f337130bf70fd0dc4af91434cd1111
nickidentivue <107918806+nickidentivue@users.noreply.github.com>
2022-07-27 16:55:43 +0100

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,8 +18,8 @@ public partial class MainPage : ContentPage
     void CreateWideButton()
     {
 
-        int numberOfColumns = 10;
-        int numberOfRows = 3;
+       int numberOfColumns = 10;
+        int numberOfRows = 10;
         int spacing = 5;
 
         double percentagePerColumn = 100d / (double)numberOfColumns;
@@ -56,15 +56,13 @@ public partial class MainPage : ContentPage
         Grid wideGrid2 = new Grid { BackgroundColor = Color.FromRgba(1f, 1f, 0.2f, 1f), Margin = 5 };
         wideGrid2.Add(new BoxView { Color = Color.FromRgba(0.5f, 1f, 0.5f, 1f), Margin = 20 });
         Grid.SetColumnSpan(wideGrid2, 6);
+        Grid.SetRowSpan(wideGrid2, 6);
         MainGrid.Add(wideGrid2, 0, 1);
 
         Grid grid = new Grid { BackgroundColor = Color.FromRgba(0.5f, 1f, 0.2f, 1f), Margin = 5 };
-        grid.Add(new Label { Margin = 20, BackgroundColor = Color.FromRgba(0.7f, 0.6f, 0f, 0.5f), Text = "Column span = 1", VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center });
-        MainGrid.Add(grid, 0,2);
-        Grid.SetColumnSpan(grid, 1);
-
-
-
+        grid.Add(new Label { Margin = 20, BackgroundColor = Color.FromRgba(0.7f, 0.6f, 0f, 0.5f), Text = "Span = 1", VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center });
+        MainGrid.Add(grid, 0,9);
+    
     }
     
   
